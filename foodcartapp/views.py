c648a0b65aa2fc62d0883a42a107be65c4dc32d9
IvanKMK <kumeyko@hotmail.com>
2022-07-04 23:26:22 +0200

    step 3/23 are done.

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from django.templatetags.static import static
+import json
 
 
 from .models import Product
@@ -58,5 +59,14 @@ def product_list_api(request):
 
 
 def register_order(request):
-    # TODO это лишь заглушка
-    return JsonResponse({})
+    try:
+        data = json.loads(request.body.decode())
+        print(data)
+        return JsonResponse({
+            'msg': data
+        })
+
+    except ValueError:
+        return JsonResponse({
+            'error': 'bla bla bla',
+        })
