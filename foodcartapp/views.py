de76c5242142ca3c07341f08a727bcf53dcda9b4
IvanKMK <kumeyko@hotmail.com>
2022-07-10 23:26:04 +0200

    Rewrite data validation logic with DRF.

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,15 +1,35 @@
 from django.http import JsonResponse
 from django.templatetags.static import static
-import json
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
 import phonenumbers
 from phonenumbers import carrier
 from phonenumbers.phonenumberutil import number_type
-
+from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ValidationError
 
 from .models import Product, Order, OrderItem
+
+
+class OrderItemSerializer(ModelSerializer):
+
+    class Meta:
+        model = OrderItem
+        fields = ['product', 'quantity']
+
+
+class OrderSerializer(ModelSerializer):
+    products = OrderItemSerializer(
+        many=True, allow_empty=False
+    )
+
+    class Meta:
+        model = Order
+        fields = [
+            'firstname', 'lastname',
+            'address', 'phonenumber',
+            'products',
+        ]
 
 
 def banners_list_api(request):
@@ -64,64 +84,48 @@ def product_list_api(request):
     })
 
 
-@api_view(['POST'])
-def register_order(request):
-    order = request.data
-    required_keys = ['firstname', 'lastname', 'phonenumber', 'address']
-
+def validate(order):
+    errors = []
     empty_keys = []
-    absent_keys = []
-    
-    for key in required_keys:
-        if key not in order.keys():
-            absent_keys.append(key)
-    if absent_keys:
-        content = {
-            ', '.join(absent_keys): 'Обязательное поле.'
-        }
-        return Response(content, status=status.HTTP_406_NOT_ACCEPTABLE)
 
     for key in order.keys():
         if order[key] is None or order[key] == "":
             empty_keys.append(key)
     if empty_keys:
-        content = {
-            ', '.join(empty_keys): 'Это поле не может быть пустым.'
-        }
-        return Response(content, status=status.HTTP_406_NOT_ACCEPTABLE)
+        errors.append(
+            {', '.join(empty_keys): 'Это поле не может быть пустым.'})
 
     if isinstance(order['firstname'], list):
-        content = {
-            'firstname': ' Not a valid string.'
-        }
-        return Response(content, status=status.HTTP_406_NOT_ACCEPTABLE)
+        errors.append({'firstname': ' Not a valid string.'})
 
     if isinstance(order['products'], list) and len(order['products']) == 0:
-        content = {
-            'products': 'Этот список не может быть пустым.'
-        }
-        return Response(content, status=status.HTTP_406_NOT_ACCEPTABLE)
+        errors.append({'products': 'Этот список не может быть пустым.'})
 
     if isinstance(order['products'], str):
-        content = {
-            'products': 'Ожидался list со значениями, но был получен "str".'
-        }
-        return Response(content, status=status.HTTP_406_NOT_ACCEPTABLE)
+        errors.append(
+            {'products': 'Ожидался list со значениями, но был получен "str".'})
 
     for ordered_product in order['products']:
         if not Product.objects.filter(id=ordered_product['product']):
-            content = {
+            errors.append({
                 'products': f'Недопустимый продукт {ordered_product["product"]}'
-            }
-            return Response(content, status=status.HTTP_406_NOT_ACCEPTABLE)
+            })
 
     if not carrier._is_mobile(
         number_type(phonenumbers.parse(order['phonenumber']))
     ):
-        content = {
-            'phonenumber': 'Введен некорректный номер телефона.'
-        }
-        return Response(content, status=status.HTTP_406_NOT_ACCEPTABLE)
+        errors.append({'phonenumber': 'Введен некорректный номер телефона.'})
+
+    if errors:
+        raise ValidationError(errors)
+
+
+@api_view(['POST'])
+def register_order(request):
+
+    order = request.data
+    serializer = OrderSerializer(data=order)
+    serializer.is_valid(raise_exception=True)
 
     customer = Order.objects.create(
         firstname=order['firstname'],
@@ -136,4 +140,5 @@ def register_order(request):
             product=Product.objects.get(id=product['product']),
             quantity=product['quantity']
         )
-    return Response(order)
+
+    return Response(serializer.data)
